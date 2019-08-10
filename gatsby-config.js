004dc16ab9fc53e89806164049caac826eb58ec9
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-10 11:40:29 -0400

    Adding blog functionality and refactoring from deprecated MDX plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,25 @@ module.exports = {
   siteMetadata: {
     title: 'Frontend Masters Gatsby Workshop',
     description:
-      'A site we built together during a full-day Frontend Masters Gatsby Workshop!',
+      'A site we built together during a full-day Frontend Masters Gatsby workshop!',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/templates/post.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
