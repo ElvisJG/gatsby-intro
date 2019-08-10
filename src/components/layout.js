c612f56311f0513014058e7f9d5e08b4b5d7d9a6
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-09 23:53:03 -0400

    Added gatsby-config to add emotion for styling, an about page, links using reach router within gatsby

---
+++ src/components/layout.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Global, css } from '@emotion/core';
+
+const Layout = ({ children }) => (
+  <>
+    <Global
+      styles={css`
+        * {
+          box-sizing: border-box;
+          margin: 0;
+        }
+        * + * {
+          margin-top: 1rem;
+        }
+
+        html,
+        body {
+          margin: 0;
+          color: #555;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Helvetica, Arial, sans-serif;
+          font-size: 18px;
+          line-height: 1.4;
+        }
+      `}
+    />
+  </>
+);
