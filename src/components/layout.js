2a4cd65001745b076b71693cd519a1f3a7b6c2fe
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-10 10:55:25 -0400

    Added use-sitemetadata hook for better SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,68 +1,79 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Helmet from 'react-helmet';
 import Header from './header';
+import useSiteMetadata from '../hooks/use-sitemetadata';
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-        }
-        /* Lobotomized owl selector, every sibling selector gets this style  */
-        * + * {
-          margin-top: 1rem;
-        }
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
 
-        html,
-        body {
-          margin: 0;
-          color: #555;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Helvetica, Arial, sans-serif;
-          font-size: 18px;
-          line-height: 1.4;
-
-          /* remove margin for the main div that Gatsby mounts into */
-          > div {
-            margin-top: 0;
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+          }
+          /* Lobotomized owl selector, every sibling selector gets this style  */
+          * + * {
+            margin-top: 1rem;
           }
 
-          h1,
-          h2,
-          h3,
-          h4,
-          h5 {
-            color: #222;
-            line-height: 1.1;
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Helvetica, Arial, sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
 
-            + * {
-              margin-top: 0.5rem;
+            /* remove margin for the main div that Gatsby mounts into */
+            > div {
+              margin-top: 0;
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5 {
+              color: #222;
+              line-height: 1.1;
+
+              + * {
+                margin-top: 0.5rem;
+              }
+            }
+
+            strong {
+              color: #222;
+            }
+
+            li {
+              margin-top: 0.25rem;
             }
           }
-
-          strong {
-            color: #222;
-          }
-
-          li {
-            margin-top: 0.25rem;
-          }
-        }
-      `}
-    />
-    <Header />
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 90vw;
-        width: 550px;
-      `}
-    >
-      {children}
-    </main>
-  </>
-);
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
 
 export default Layout;
